75b0784260a684b998680ed9402009decbc9e2ca
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-19 21:31:35 -0200

    Update day17.lua
    part II done finaly

--- day17.lua
+++ day17.lua
@@ -2,30 +2,35 @@ array = {}
 array[0] = 0
 curpos = 0
 step = 394
-for i = 1, 50000000 do
+for i = 1, 2017 do
     curpos = (curpos + step)%(#array+1)
-    if curpos < #array then
-        for j = #array, curpos + 1,-1 do
-            array[j + 1] = array[j]
-        end
-    end
-    array[curpos + 1] = i
+    table.insert(array,curpos + 1,i)
     curpos = curpos + 1
-    if i == 2017 then
-        for i,s in pairs(array) do
-            if s == 2017 then
-                print("part I")
-                print(array[i+1])
-                break
-            end
-        end
-    elseif i == 50000000 then
-        for i,s in pairs(array) do
-            if s == 0 then
-                print("part II")
-                print(array[i+1])
-                break
-            end
-        end
+end
+for i,s in pairs(array) do
+    if s == 2017 then
+        print("part I(" .. i .. ")")
+        print(array[i+1])
+        break
+    end
+end
+array = {}
+array[0] = 0
+curpos = 0
+zeropos = 0
+for i = 1, 50000000 do
+    curpos = (curpos + step)%i
+    if curpos == zeropos then
+        table.insert(array,zeropos + 1,i)
+    elseif curpos < zeropos then
+        table.insert(array,zeropos - 1,i)
+    end
+    curpos = curpos + 1
+end
+for i,s in pairs(array) do
+    if s == 0 then
+        print("part II(" .. i .. ")")
+        print(array[i+1])
+        break
     end
 end
