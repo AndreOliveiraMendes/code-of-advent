9a5703126ba99747f5fa5a54688161620ad31ef6
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-21 22:18:21 -0200

    Update day18.lua

--- day18.lua
+++ day18.lua
@@ -1,4 +1,4 @@
-function snd(v,vlist,slist)
+function snd(v, vlist, slist)
    local value
    if type(v) == "number" then
       value = v
@@ -7,48 +7,48 @@ function snd(v,vlist,slist)
    end
    table.insert(slist, value)
 end
-function rcv(v,vlist,slist)
+function rcv(v, vlist, slist)
    local value
    if type(v) == "number" then
       value = v
    else
       value = vlist[v]
    end
-   if v~=0 then
+   if v ~= 0 then
       return slist[#slist]
    else
       return false
    end
 end
-function set(v1,v2,list)
+function set(v1, v2, list)
    if type(v2) == "number" then
       list[v1] = v2
    else
       list[v1] = list[v2]
    end
 end
-function add(v1,v2,list)
+function add(v1, v2, list)
    if type(v2) == "number" then
       list[v1] = list[v1] + v2
    else
       list[v1] = list[v1] + list[v2]
    end
 end
-function mul(v1,v2,list)
+function mul(v1, v2, list)
    if type(v2) == "number" then
       list[v1] = list[v1] * v2
    else
       list[v1] = list[v1] * list[v2]
    end
 end
-function mod(v1,v2,list)
+function mod(v1, v2, list)
    if type(v2) == "number" then
       list[v1] = list[v1] % v2
    else
       list[v1] = list[v1] % list[v2]
    end
 end
-function jgz(v1,v2,list,i)
+function jgz(v1, v2, list, i)
    local com, val
    if type(v1) == "number" then
       com = v1
@@ -107,13 +107,13 @@ input = {{set,"i",31},
 {snd,"a"},
 {jgz,"f",-16},
 {jgz,"a",-19}}
-function init(t,v)
+function init(t, v, id)
    if type(v) == "number" then return end
-   if not t[v] then t[v] = 0 end
+   if not t[v] then t[v] = id end
 end
-function isfunc(f,...)
+function isfunc(f, ...)
    local arg = {...}
-   for i,fs in pairs(arg) do
+   for i, fs in pairs(arg) do
       if f == fs then return true end
    end
    return false
@@ -121,27 +121,27 @@ end
 i = 1
 varlist = {}
 sound = {}
-while i>=1 and i<=#input do
+while i >= 1 and i <= #input do
    local f, v1, v2 = table.unpack(input[i])
-   if v1 then init(varlist,v1) end
-   if v2 then init(varlist,v2) end
+   if v1 then init(varlist, v1, 0) end
+   if v2 then init(varlist, v2, 0) end
    if isfunc(f,set,add,mul,mod) then
-      f(v1,v2,varlist)
+      f(v1, v2, varlist)
    elseif isfunc(f,snd,rcv) then
       local rec = f(v1,varlist,sound)
       if rec then
-         print("part I, recoverd:",rec)
+         print("part I, recoverd:", rec)
          break
       end
    end
-   if isfunc(f,jgz) then
+   if isfunc(f, jgz) then
       i = jgz(v1,v2,varlist,i)
    else
       i = i + 1
    end
 end
 print("variables x content")
-for i,s in pairs(varlist) do
+for i, s in pairs(varlist) do
    print(i,s)
 end
 print("sounds played")
