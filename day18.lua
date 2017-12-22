34aed1ab182ec9141dd2d22674c63acbb32e4f79
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-22 01:17:48 -0200

    Update day18.lua
    part II incomplet

--- day18.lua
+++ day18.lua
@@ -1,70 +1,70 @@
 function snd(v, vlist, slist)
-   local value
-   if type(v) == "number" then
-      value = v
-   else
-      value = vlist[v]
-   end
-   table.insert(slist, value)
+    local value
+    if type(v) == "number" then
+        value = v
+    else
+        value = vlist[v]
+    end
+    table.insert(slist, value)
 end
 function rcv(v, vlist, slist)
-   local value
-   if type(v) == "number" then
-      value = v
-   else
-      value = vlist[v]
-   end
-   if v ~= 0 then
-      return slist[#slist]
-   else
-      return false
-   end
+    local value
+    if type(v) == "number" then
+        value = v
+    else
+        value = vlist[v]
+    end
+    if v ~= 0 then
+        return slist[#slist]
+    else
+        return false
+    end
 end
 function set(v1, v2, list)
-   if type(v2) == "number" then
-      list[v1] = v2
-   else
-      list[v1] = list[v2]
-   end
+    if type(v2) == "number" then
+        list[v1] = v2
+    else
+        list[v1] = list[v2]
+    end
 end
 function add(v1, v2, list)
-   if type(v2) == "number" then
-      list[v1] = list[v1] + v2
-   else
-      list[v1] = list[v1] + list[v2]
-   end
+    if type(v2) == "number" then
+        list[v1] = list[v1] + v2
+    else
+        list[v1] = list[v1] + list[v2]
+    end
 end
 function mul(v1, v2, list)
-   if type(v2) == "number" then
-      list[v1] = list[v1] * v2
-   else
-      list[v1] = list[v1] * list[v2]
-   end
+    if type(v2) == "number" then
+        list[v1] = list[v1] * v2
+    else
+        list[v1] = list[v1] * list[v2]
+    end
 end
 function mod(v1, v2, list)
-   if type(v2) == "number" then
-      list[v1] = list[v1] % v2
-   else
-      list[v1] = list[v1] % list[v2]
-   end
+    if type(v2) == "number" then
+        list[v1] = list[v1] % v2
+    else
+        list[v1] = list[v1] % list[v2]
+    end
 end
 function jgz(v1, v2, list, i)
-   local com, val
-   if type(v1) == "number" then
-      com = v1
-   else
-      com = list[v1]
-   end
-   if type(v2) == "number" then
-      val = v2
-   else
-      val = list[v2]
-   end
-   if com > 0 then
-      return i + val
-   else
-      return i + 1
-   end
+    local com, val
+    if type(v1) == "number" then
+        com = v1
+    else
+        com = list[v1]
+    end
+    if type(v2) == "number" then
+        val = v2
+    else
+        val = list[v2]
+    end
+    if com > 0 then
+        return i + val
+    else
+        return i + 1
+    end
 end
 input = {{set,"i",31},
 {set,"a",1},
@@ -108,43 +108,50 @@ input = {{set,"i",31},
 {jgz,"f",-16},
 {jgz,"a",-19}}
 function init(t, v, id)
-   if type(v) == "number" then return end
-   if not t[v] then t[v] = id end
+    if type(v) == "number" then return end
+    if not t[v] then t[v] = id end
 end
 function isfunc(f, ...)
-   local arg = {...}
-   for i, fs in pairs(arg) do
-      if f == fs then return true end
-   end
-   return false
+    local arg = {...}
+    for i, fs in pairs(arg) do
+        if f == fs then return true end
+    end
+    return false
 end
+--part I
 i = 1
 varlist = {}
 sound = {}
 while i >= 1 and i <= #input do
-   local f, v1, v2 = table.unpack(input[i])
-   if v1 then init(varlist, v1, 0) end
-   if v2 then init(varlist, v2, 0) end
-   if isfunc(f,set,add,mul,mod) then
-      f(v1, v2, varlist)
-   elseif isfunc(f,snd,rcv) then
-      local rec = f(v1,varlist,sound)
-      if rec then
-         print("part I, recoverd:", rec)
-         break
-      end
-   end
-   if isfunc(f, jgz) then
-      i = jgz(v1,v2,varlist,i)
-   else
-      i = i + 1
-   end
+    local f, v1, v2 = table.unpack(input[i])
+    if v1 then init(varlist, v1, 0) end
+    if v2 then init(varlist, v2, 0) end
+    if isfunc(f,set,add,mul,mod) then
+        f(v1, v2, varlist)
+    elseif isfunc(f,snd,rcv) then
+        local rec = f(v1,varlist,sound)
+        if rec then
+            print("part I, recoverd:", rec)
+            break
+        end
+    end
+    if isfunc(f, jgz) then
+        i = jgz(v1,v2,varlist,i)
+    else
+        i = i + 1
+    end
 end
-print("variables x content")
-for i, s in pairs(varlist) do
-   print(i,s)
+--- part II
+function rcvn(v, vlist, slist)
+    if #slist == 0 then
+        return false
+    else
+        vlist[v] = slist[1]
+        table.remove(slist,1)
+    end
 end
-print("sounds played")
-for i,s in pairs(sound) do
-   print(i,s)
+for i, s in pairs(input) do
+    if s[1] == rcv then
+        input[i][1] = rcvn
+    end
 end
