91debbea800472c3487435b68e4b263278e237b0
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-24 16:01:34 -0200

    Update day21.lua
    part II incomplet

--- day21.lua
+++ day21.lua
@@ -1,10 +1,10 @@
-rule = {{"../..",".../#.#/..."},
+rule2 = {{"../..",".../#.#/..."},
 {"#./..","..#/..#/#.."},
 {"##/..",".../#../..#"},
 {".#/#.","#../.../..."},
 {"##/#.","#.#/.#./#.."},
-{"##/##","..#/#.#/..#"},
-{".../.../...",".#../#..#/#.../.#.."},
+{"##/##","..#/#.#/..#"}}
+rule3 = {{".../.../...",".#../#..#/#.../.#.."},
 {"#../.../...","..##/..##/.#.#/...."},
 {".#./.../...","..##/..##/.###/##.."},
 {"##./.../...","..../.##./#.##/..#."},
@@ -177,7 +177,54 @@ function flip(art) -- flip a art
         end
     end
 end
-function artbreak(art,n) -- break a art in multiple small arts
+function enchantaux(simp,s1,s2)
+    return (simp == s1) or (simp == s2)
+end
+function action(obj,f,...)
+    if #{...} > 0 then
+        f(obj)
+        action(obj,...)
+    else
+        f(obj)
+    end
+end
+function enchant(art, n)
+    --[[
+        art1 = 0
+        art2 = 0F
+        art3 = 90
+        art4 = 90F
+        art5 = 180
+        art6 = 180F
+        art7 = 270
+        art8 = 270F
+    --]]
+    local art1 = copy(art)
+    action(art1,flip)
+    local eartsimp
+    repeat
+        local sart, sart1 = tosimplyfied(art), tosimplyfied(art1)
+        if n == 2 then
+            for i, s in pairs(rule2) do
+                if enchantaux(s[1], sart, sart1) then
+                    eartsimp = s[2]
+                    break
+                end
+            end
+        else
+            for i, s in pairs(rule3) do
+                if enchantaux(s[1], sart, sart1) then
+                    eartsimp = s[2]
+                    break
+                end
+            end
+        end
+        action(art,rotate)
+        action(art1,rotate)
+    until eartsimp
+    return toart(eartsimp)
+end
+function artbreak(art,n) -- break a art in multiple small arts (and also enchant now)
     local max = #art/n
     local t = {}
     for i = 1, max do
@@ -188,9 +235,10 @@ function artbreak(art,n) -- break a art in multiple small arts
                 if not nart[xr] then nart[xr] = {} end
                 for y = n*(j - 1) + 1, n*j do
                     local yr = (y - 1)%n + 1
-                    nart[xr][yr] = art[x][y]
+                    nart[xr][yr], art[x][y] = art[x][y], nil
                 end
             end
+            nart = enchant(nart,n)
             table.insert(t,nart)
         end
     end
@@ -251,31 +299,23 @@ function toart(simp) --convert simplified version to art
     end
     return t1
 end
+function tm1(t1,t2) -- merges type 1
+    for i = 1, #t1 do
+        for j, s in pairs(t2[i]) do
+            table.insert(t1[i],s)
+            t2[i] = nil
+        end
+    end
+    t2 = nil
+end
+function tm2(t1,t2) -- merges type 2
+    local size = #t1
+    for i = #t1 + 1, #t1 + #t2 do
+        t1[i], t2[i - size] = t2[i - size], nil
+    end
+    t2 = nil
+end
 function artjoin(list) -- join multiples art in one
-    local function tm1(t1,t2) -- merges type 1
-        local t = {}
-        for i = 1, #t1 do
-            t[i] = {}
-            for j, s in pairs(t1[i]) do
-                table.insert(t[i],s)
-            end
-            for j, s in pairs(t2[i]) do
-                table.insert(t[i],s)
-            end
-        end
-        return t
-    end
-    local function tm2(t1,t2) -- merges type 2
-        local t = {}
-        for i = 1, #t1 + #t2 do
-            if i <= #t1 then
-                t[i] = t1[i]
-            else
-                t[i] = t2[i - #t1]
-            end
-        end
-        return t
-    end
     local max = math.sqrt(#list) -- simetry, also n^2 = #list
     local nart
     k = 0
@@ -287,71 +327,17 @@ function artjoin(list) -- join multiples art in one
                 nsart = list[k]
             else
                 k = k + 1
-                nsart = tm1(nsart,list[k])
+                tm1(nsart,list[k])
             end
         end
         if not nart then
             nart = nsart
         else
-            nart = tm2(nart,nsart)
+            tm2(nart,nsart)
         end
     end
+    k = nil
     return nart
-end
-function enchantaux(simp,t)
-    for i, s in pairs(t) do
-        if s == simp then
-            return true
-        end
-    end
-    return false
-end
-function action(obj,f,...)
-    if #{...} > 0 then
-        f(obj)
-        action(obj,...)
-    else
-        f(obj)
-    end
-end
-function enchant(art)
-    --[[
-        art1 = 0
-        art2 = 0F
-        art3 = 90
-        art4 = 90F
-        art5 = 180
-        art6 = 180F
-        art7 = 270
-        art8 = 270F
-    --]]
-    local art1 = copy(art)
-    local art2 = copy(art)
-    local art3 = copy(art)
-    local art4 = copy(art)
-    local art5 = copy(art)
-    local art6 = copy(art)
-    local art7 = copy(art)
-    local art8 = copy(art)
-    action(art2,flip)
-    action(art3,rotate)
-    action(art4,rotate,flip)
-    action(art5,rotate,rotate)
-    action(art6,rotate,rotate,flip)
-    action(art7,rotate,rotate,rotate)
-    action(art8,rotate,rotate,rotate,flip)
-    local t = {art1, art2, art3, art4, art5, art6, art7, art8}
-    for i, s in pairs(t) do
-        t[i] = tosimplyfied(s)
-    end
-    local eartsimp
-    for i, s in pairs(rule) do
-        if enchantaux(s[1],t) then
-            eartsimp = s[2]
-            break
-        end
-    end
-    return toart(eartsimp)
 end
 function count(art) -- count how many pixels on a art have
     local count = 0
@@ -364,21 +350,20 @@ function count(art) -- count how many pixels on a art have
     end
     return count
 end
-p1 = 0
-for i = 1, 5 do
-    local list
+for i = 1, 18 do
+    print("process " .. i)
     if #art % 2 == 0 then
         list = artbreak(art,2)
     elseif #art % 3 == 0 then
         list = artbreak(art,3)
     end
-    for i, s in pairs(list) do
-        list[i] = enchant(s)
-    end
     art = artjoin(list)
+    list = nil
     if i == 5 then
-        p1 = count(art)
+        print("part I")
+        print("there has " .. count(art) .. " activated pixels")
+    elseif i == 18 then
+        print("part II")
+        print("there has " .. count(art) .. " activated pixels")
     end
 end
-print("part I")
-print("there has " .. p1 .. " activated pixels")
