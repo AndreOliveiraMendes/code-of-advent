6a2e827205798ab0c7ad6ade6cafb18ce27dec75
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-24 01:43:55 -0200

    Update day21.lua
    part I finished

--- day21.lua
+++ day21.lua
@@ -171,9 +171,9 @@ function rotate(art) -- rotate a art anti - clockwize
     end
 end
 function flip(art) -- flip a art
-    for x = 1, #art do
+    for x = 1, #art/2 do
         for y = 1, #art do
-            art[x][y], art[#art + 1 - x] = art[#art + 1 - x][y]
+            art[x][y], art[#art + 1 - x][y] = art[#art + 1 - x][y], art[x][y]
         end
     end
 end
@@ -196,11 +196,189 @@ function artbreak(art,n) -- break a art in multiple small arts
     end
     return t
 end
+function trans(input)
+    if input == 0 then
+        return "."
+    elseif input == 1 then
+        return "#"
+    elseif input == "." then
+        return 0
+    elseif input == "#" then
+        return 1
+    else
+        return input
+    end
+end
+function tosimplyfied(art) --convert a art to simplified version
+    local str = ""
+    for i = 1, # art do
+        for j = 1, # art do
+            if (j == #art) and (i < #art) then
+                str = str .. trans(art[j][i]) .. "/"
+            else
+                str = str .. trans(art[j][i])
+            end
+        end
+    end
+    return str
+end
+function toart(simp) --convert simplified version to art
+    local t1 = {}
+    local i = 1
+    local bar = string.find(simp, "/", i)
+    while bar do
+        local t2 = {}
+        local str = string.sub(simp, i, bar - 1)
+        for j = 1, #str do
+            local fstr = string.sub(str, j, j)
+            table.insert(t2,trans(fstr))
+        end
+        table.insert(t1,t2)
+        i = bar + 1
+        bar = string.find(simp, "/", i)
+    end
+    local t2 = {}
+    local str = string.sub(simp, i, #simp)
+    for j = 1, #str do
+        local fstr = string.sub(str, j, j)
+        table.insert(t2,trans(fstr))
+    end
+    table.insert(t1,t2)
+    for x = 1, #t1 do
+        for y = x, #t1 do
+            t1[x][y], t1[y][x] = t1[y][x], t1[x][y]
+        end
+    end
+    return t1
+end
 function artjoin(list) -- join multiples art in one
+    local function tm1(t1,t2) -- merges type 1
+        local t = {}
+        for i = 1, #t1 do
+            t[i] = {}
+            for j, s in pairs(t1[i]) do
+                table.insert(t[i],s)
+            end
+            for j, s in pairs(t2[i]) do
+                table.insert(t[i],s)
+            end
+        end
+        return t
+    end
+    local function tm2(t1,t2) -- merges type 2
+        local t = {}
+        for i = 1, #t1 + #t2 do
+            if i <= #t1 then
+                t[i] = t1[i]
+            else
+                t[i] = t2[i - #t1]
+            end
+        end
+        return t
+    end
     local max = math.sqrt(#list) -- simetry, also n^2 = #list
-    local nart = {}
+    local nart
+    k = 0
+    for i = 1, max do
+        local nsart
+        for j = 1, max do
+            if not nsart then
+                k = k + 1
+                nsart = list[k]
+            else
+                k = k + 1
+                nsart = tm1(nsart,list[k])
+            end
+        end
+        if not nart then
+            nart = nsart
+        else
+            nart = tm2(nart,nsart)
+        end
+    end
     return nart
 end
-for i = 1, 1 do
-    print("process " .. i)
+function enchantaux(simp,t)
+    for i, s in pairs(t) do
+        if s == simp then
+            return true
+        end
+    end
+    return false
 end
+function action(obj,f,...)
+    if #{...} > 0 then
+        f(obj)
+        action(obj,...)
+    else
+        f(obj)
+    end
+end
+function enchant(art)
+    --[[
+        art1 = 0
+        art2 = 0F
+        art3 = 90
+        art4 = 90F
+        art5 = 180
+        art6 = 180F
+        art7 = 270
+        art8 = 270F
+    --]]
+    local art1 = copy(art)
+    local art2 = copy(art)
+    local art3 = copy(art)
+    local art4 = copy(art)
+    local art5 = copy(art)
+    local art6 = copy(art)
+    local art7 = copy(art)
+    local art8 = copy(art)
+    action(art2,flip)
+    action(art3,rotate)
+    action(art4,rotate,flip)
+    action(art5,rotate,rotate)
+    action(art6,rotate,rotate,flip)
+    action(art7,rotate,rotate,rotate)
+    action(art8,rotate,rotate,rotate,flip)
+    local t = {art1, art2, art3, art4, art5, art6, art7, art8}
+    for i, s in pairs(t) do
+        t[i] = tosimplyfied(s)
+    end
+    local eartsimp
+    for i, s in pairs(rule) do
+        if enchantaux(s[1],t) then
+            eartsimp = s[2]
+            break
+        end
+    end
+    return toart(eartsimp)
+end
+function count(art) -- count how many pixels on a art have
+    local count = 0
+    for i = 1, #art do
+        for j = 1, #art do
+            if art[i][j] == 1 then
+                count = count + 1
+            end
+        end
+    end
+    return count
+end
+p1 = 0
+for i = 1, 5 do
+    local list
+    if #art % 2 == 0 then
+        list = artbreak(art,2)
+    elseif #art % 3 == 0 then
+        list = artbreak(art,3)
+    end
+    for i, s in pairs(list) do
+        list[i] = enchant(s)
+    end
+    art = artjoin(list)
+    if i == 5 then
+        p1 = count(art)
+    end
+end
+print("part I")
+print("there has " .. p1 .. " activated pixels")
