caa20d8766c7be4c68cb1b0d527942cb7c498ac1
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-27 16:17:24 -0200

    Update day2.lua
    part II

--- 2015/day2.lua
+++ 2015/day2.lua
@@ -1012,10 +1012,14 @@ for digit in string.gmatch(input, "%d+") do
         table.insert(presents, aux)
     end
 end
-sum = 0
+area = 0
+com = 0
 for i, s in pairs(presents) do
     local l, w, h = s[1], s[2], s[3]
-    sum = sum + 2*l*w + 2*l*h + 2*w*h + math.min(l*w,l*h,w*h)
+    area = area + 2*l*w + 2*l*h + 2*w*h + math.min(l*w,l*h,w*h)
+    com = com + math.min(2*(l+w),2*(l+h),2*(w+h)) + l*h*w
 end
 --part I
-print("the total square feet of wrapping paper they should buy is " .. sum)
+print("the total square feet of wrapping paper they should buy is " .. area)
+--part II
+print("the total feet of ribbon they should buy is " .. com)
