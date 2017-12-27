215769b91c6b09ce0735c3a134ae958f05b7058f
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-27 20:36:42 -0200

    Update day6.lua
    part II

--- 2015/day6.lua
+++ 2015/day6.lua
@@ -300,6 +300,7 @@ turn on 31,760 through 655,892
 toggle 628,958 through 811,992]]
 action = {}
 light = {}
+light2 = {}
 for line in string.gmatch(input, "%C+") do
     table.insert(action, line)
 end
@@ -323,12 +324,17 @@ for i, s in pairs(action) do
         for y = ymin, ymax do
             if not light[x] then light[x] = {} end
             if not light[x][y] then light[x][y] = 0 end
+            if not light2[x] then light2[x] = {} end
+            if not light2[x][y] then light2[x][y] = 0 end
             if act == "turn on" then
                 light[x][y] = 1
+                light2[x][y] = light2[x][y] + 1
             elseif act == "turn off" then
                 light[x][y] = 0
+                light2[x][y] = math.max(light2[x][y] - 1, 0)
             elseif act == "toggle" then
                 light[x][y] = 1 - light[x][y]
+                light2[x][y] = light2[x][y] + 2
             else
                 print("error: no action found")
             end
@@ -336,12 +342,16 @@ for i, s in pairs(action) do
     end
 end
 count = 0
+bright = 0
 for x = 0, 999 do
     for y = 0, 999 do
         if light[x] and light[x][y] then
             count = count + light[x][y]
+            bright = bright + light2[x][y]
         end
     end
 end
 --part I
 print("there " .. count .. " light(s) on")
+--part II
+print("the total bright is " .. bright)
