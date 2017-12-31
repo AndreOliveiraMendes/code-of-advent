0bedacf17e32cedcba2c8080a050eb8e0fe269e1
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-12-31 21:57:02 -0200

    Update day18.lua
    part II

--- 2015/day18.lua
+++ 2015/day18.lua
@@ -98,7 +98,7 @@ input = [[#...##......#......##.##..#...##......##.#.#.###.#.#..#..#......####..
 #..#.#..#...###.#..##.#...#...##.#......#...#..#..####..##.....#.###...#.#..#.#....#.#####.##.###...
 ###....#.#..#.#..###..#.##......#...#..#..##.#..###..##..#..#.####..#...########..##.#.##.#.#.#...#.
 .#.#.##.##.###..#...#.#....#..#.##..#.#.#.#.##.##.#####...#........####..###..####.#####..#.##.#.##.]]
-x, y, ml2 = 1, 1, {}
+x, y, ml2, ml2d = 1, 1, {}, {}
 for line in string.gmatch(input, "%C+") do
     ml2[y] = {}
     for i = 1, #line do
@@ -110,6 +110,16 @@ for line in string.gmatch(input, "%C+") do
         x = x + 1
     end
     y, x = y + 1, 1
+end
+for y = 1, #ml2 do
+    ml2d[y] = {}
+    for x = 1, #ml2[y] do
+        if (x == 1 or x == #ml2[#ml2]) and (y == 1 or y == #ml2) then
+            ml2d[y][x] = 1
+        else
+            ml2d[y][x] = ml2[y][x]
+        end
+    end
 end
 function pml(ml, t) --Print Matrix Light
     if t then
@@ -137,14 +147,25 @@ function f(x, y, ml)
         return 0
     end
 end
-function us(x, y, ml)
-    local state, count = ml[y][x], 0
+function g(x, y, ml)
+    if ml[y] and ml[y][x] then
+        return 1
+    else
+        return 0
+    end
+end
+function us(x, y, ml, def)
+    local state, count, ncount = ml[y][x], 0, 0
     for xr = x - 1, x + 1 do
         for yr = y - 1, y + 1 do
             if xr ~= x or yr ~= y then
                 count = count + f(xr, yr, ml)
+                ncount = ncount + g(xr, yr, ml)
             end
         end
+    end
+    if def then
+        if ncount == 3 then return 1 end
     end
     if state == 1 then
         if count == 2 or count == 3 then
@@ -171,5 +192,19 @@ for t = 1, 100 do
     ml2 = temp
     if t == 100 then
         print("after " .. t .. " steps, there are " .. gloml(ml2) .. " light on")
+    end
+end
+--part II
+for t = 1, 100 do
+    local temp = {}
+    for y = 1, #ml2d do
+        temp[y] = {}
+        for x = 1, #ml2d[y] do
+            temp[y][x] = us(x, y, ml2d, true)
+        end
+    end
+    ml2d = temp
+    if t == 100 then
+        print("after " .. t .. " steps, there are " .. gloml(ml2d) .. " light on")
     end
 end
