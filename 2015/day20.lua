d8a84b2c134aca09983437d65a71261e330cfa26
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-01 01:55:49 -0200

    Update day20.lua
    part II

--- 2015/day20.lua
+++ 2015/day20.lua
@@ -1,20 +1,103 @@
-input = 29000000
+--[[
+    function d(n)
+        local sum = 0
+        for i = 1, n do
+            if n % i == 0 then
+                sum = sum + i
+            end
+        end
+        return sum
+    end
+    function dm(n)
+        local sum = 0
+        for i = 1, n do
+            if n <= 50*i and n%i == 0 then
+                sum = sum + i
+            end
+        end
+        return sum
+    end
+    function d(n)
+        local sum = 0
+        for i = 1, math.sqrt(n) do
+            if n % i == 0 then
+                if i == math.sqrt(n) then
+                    sum = sum + i
+                else
+                    sum = sum + i + n/i
+                end
+            end
+        end
+        return sum
+    end
+    function dm(n)
+        local sum = 0
+        for i = n, math.sqrt(n), -1 do
+            if n <= 50*i and n%i == 0 then
+                if i == math.sqrt(n) then
+                    sum = sum + i
+                else
+                    sum = sum + i
+                    local j = n/i
+                    if n <= 5*j then
+                        sum = sum + j
+                    end
+                end
+            end
+        end
+        return math.floor(sum)
+    end
+--]]
 function d(n)
-    local sum = 0
+    local sum1, sum2 = 0, 0
     for i = 1, math.sqrt(n) do
         if n % i == 0 then
             if i == math.sqrt(n) then
-                sum = sum + i
+                sum1 = sum1 + i
+                if n <= 50*i then
+                    sum2 = sum2 + i
+                end
             else
-                sum = sum + i + n/i
+                local j = n/i
+                sum1 = sum1 + i + j
+                if n <= 50*i then
+                    sum2 = sum2 + i
+                end
+                if n <= 50*j then
+                    sum2 = sum2 + j
+                end
             end
         end
     end
-    return sum
+    return sum1, sum2
 end
-function f(n) return 10*d(n) end
+input = 29000000
+dm = {}
+function f1(n)
+    if not dm[n] then
+        local d1, d2 = d(n)
+        table.insert(dm, {d1, d2})
+        return 10*d1
+    else
+        return 10*dm[n][1]
+    end
+end
+function f2(n)
+    if not dm[n] then
+        local d1, d2 = d(n)
+        table.insert(dm, {d1, d2})
+        return 11*d2
+    else
+        return 11*dm[n][2]
+    end
+end
 x = 1
-while f(x) < input do
+while not x1 or not x2 do
+    if not x1 and f1(x) >= input then x1 = x end
+    if not x2 and f2(x) >= input then x2 = x end
     x = x + 1
 end
-print("the lowest house number to get as that many gift is " .. x)
+--part I
+print("the lowest house number to get as that many gift is " .. x1)
+--part II
+print("the lowest house number to get as that many gift after new elves plan is " .. x2)
