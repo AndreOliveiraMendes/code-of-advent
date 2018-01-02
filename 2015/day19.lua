75cd92bf590acae1d5bef705a63163613d173880
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-02 15:02:40 -0200

    Update day19.lua
    part II

--- 2015/day19.lua
+++ 2015/day19.lua
@@ -68,14 +68,59 @@ for i = 1, #temp, 2 do
 end
 temp = nil
 distincts_mol = {}
-for i, s in pairs(elemental_replace) do
-    local s_in, s_out = s[1], s[2]
-    local i, o = input_element:find(s_in)
-    while i do
-        local mol = replace_char(i, o, input_element, s_out)
-        tinsertr(distincts_mol, mol)
-        i, o = input_element:find(s_in, o + 1)
+function replace(distincts_mol, input_element)
+    for i, s in pairs(elemental_replace) do
+        local s_in, s_out = s[1], s[2]
+        local i, o = input_element:find(s_in)
+        while i do
+            local mol = replace_char(i, o, input_element, s_out)
+            tinsertr(distincts_mol, mol)
+            i, o = input_element:find(s_in, o + 1)
+        end
     end
 end
 --part I
+replace(distincts_mol, input_element)
 print("there are " .. #distincts_mol .. " distincts molecules possible")
+for element in string.gmatch(input_element, "%u%l?") do
+    tinsertr(elements, element)
+end
+print("<===========================================================>")
+print("there are " .. #elements .. " elements in this puzzle")
+a = nil
+print("<===========================================================>")
+elements_count = {}
+for i, s in pairs(elements) do
+    local count = 0
+    for k in string.gmatch(input_element, s) do
+        count = count + 1
+    end
+    elements_count[s] = count
+end
+elements_count["C"] = elements_count["C"] - elements_count["Ca"]
+count = 0
+for i, s in pairs(elements_count) do
+    count = count + s
+    print(i, s)
+end
+print("total", count)
+print("estimative:" .. count - elements_count["Rn"] - elements_count["Ar"] - 2*elements_count["Y"] - 1)
+--[[
+    Al => ThF, ThRnFAr
+    B => BCa, TiB, TiRnFAr
+    Ca => CaCa, PB, PRnFAr, SiRnFYFAr, SiRnMgAr, SiTh
+    F => CaF, PMg, SiAl
+    H => CRnAlAr, CRnFYFYFAr, CRnFYMgAr, CRnMgYFAr, HCa, NRnFYFAr, NRnMgAr, NTh, OB, ORnFAr
+    Mg => BF, TiMg
+    N => CRnFAr, HSi
+    O => CRnFYFAr, CRnMgAr, HP, NRnFAr, OTi
+    P => CaP, PTi, SiRnFAr
+    Si => CaSi
+    Th => ThCa
+    Ti => BP, TiTi
+    e => HF, NAl, OMg
+    C: ... CRn ... Ar
+    Rn: ... Rn .. Ar
+    Ar: ... Rn .. Ar
+    Y: ... Rn ... Y ... Ar
+--]]
