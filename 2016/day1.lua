7ddda2707fadabccbe9eaebd00a4e344ab463ea6
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-05 21:14:44 -0200

    Update day1.lua
    part II

--- 2016/day1.lua
+++ 2016/day1.lua
@@ -35,12 +35,22 @@ function move(x, y, dir, dl)
     end
     return x, y
 end
+visit = {}
+visit[0] = {[0] = 1}
 for i, s in pairs(moves) do
     local rot, amount = s[1], s[2]
     dir = rotate(dir, rot)
-    x, y = move(x, y, dir, amount)
+    for j = 1, amount do
+        x, y = move(x, y, dir, 1)
+        if not visit[x] then visit[x] = {} end
+        if not visit[x][y] then visit[x][y] = 1 else visit[x][y] = visit[x][y] + 1 end
+        if not xl and not yl and visit[x][y] > 1 then
+            xl, yl = x, y
+        end
+    end
 end
 function d2(x, y)
     return math.abs(x) + math.abs(y)
 end
 print("stoped " .. d2(x, y) .. " blocks away")
+print("the first location you visited twice is " .. d2(xl, yl) .. " blocks away")
