d66c8b4e7898e436406cc8eecb254f0691dea1ee
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-14 20:51:51 -0200

    Update day16.lua
    part II finished

--- 2016/day16.lua
+++ 2016/day16.lua
@@ -1,70 +1,71 @@
 input = "11011110011011101"
-inputinv = ""
-for i = #input, 1, -1 do
-    inputinv = inputinv .. tostring(1 - tonumber(input:sub(i, i)))
+function xnor1(x1, x2)
+    return 1&(~(x1 ~ x2))
 end
-size = 272
-size2 = 35651584
-joiner = {}
-for i = 1, size2/(#input+1) do
-    if not m or i <= m then
-        joiner[i] = 0
-        if not m then m = 2 end
-    else
-        joiner[i] = 1-joiner[2*m - i]
-        if 2*m <= i + 1 then
-            m = 2*m
-        end
-    end
-end
-function f(a, b, n, s)
-    local fstr = ""
-    local t = {}
-    local st = "a"
-    local i = 1
-    while #fstr <= n do
-        if st == "a" then
-            fstr = fstr .. a
-            st = "cb"
-        elseif st == "b" then
-            fstr = fstr .. b
-            st = "ca"
+function f(a, n)
+    local joiner = {}
+    local m
+    for i = 1, n/(#input+1) do
+        if not m or i <= m then
+            joiner[i] = 0
+            if not m then m = 2 end
         else
-            fstr = fstr .. s[i]
-            i = i + 1
-            st = st:sub(2, 2)
+            joiner[i] = 1-joiner[2*m - i]
+            if 2*m <= i + 1 then
+                m = 2*m
+            end
         end
     end
-    return fstr:sub(1, n)
+    local ta, tb = {}, {}
+    for i = 1, #a do
+        ta[i] = tonumber(a:sub(i, i))
+    end
+    for i = 1, #a do
+        tb[i] = 1 - ta[#a - i + 1]
+    end
+    local k = 0
+    while true do
+        if n%(2^(k + 1)) == 0 then
+            k = k + 1
+        else
+            break
+        end
+    end
+    local t = {}
+    local JI = 1
+    for i = 1, k do
+        if i == 1 then
+            for j = 1, n, 2 do
+                local p = (j - 1)%(#ta + #tb + 2) + 1
+                if p < #ta then
+                    table.insert(t, xnor1(ta[p], ta[p + 1]))
+                elseif p == #ta then
+                    table.insert(t, xnor1(ta[p], joiner[JI]))
+                    JI = JI + 1
+                elseif p == #ta + 1 then
+                    table.insert(t, xnor1(joiner[JI], tb[1]))
+                    JI = JI + 1
+                elseif p < #ta + #tb + 1 then
+                    table.insert(t, xnor1(tb[p - #ta - 1], tb[p + 1 - #ta - 1]))
+                elseif p == #ta + #tb + 1 then
+                    table.insert(t, xnor1(tb[p - #ta - 1], joiner[JI]))
+                    JI = JI + 1
+                elseif p == #ta + #tb + 2 then
+                    table.insert(t, xnor1(joiner[JI], ta[1]))
+                    JI = JI + 1
+                end
+            end
+        else
+            local a = {}
+            for j = 1, #t, 2 do
+                table.insert(a, xnor1(t[j], t[j +1]))
+            end
+            t = a
+        end
+    end
+    return table.concat(t)
 end
-function g(d1, d2)
-    d1, d2 = tonumber(d1), tonumber(d2)
-    if d1 == d2 then
-        return 1
-    else
-        return 0
-    end
-end
-output = f(input, inputinv, size, joiner)
-print("input is:\n" .. input)
-print("output after process:\n" .. output)
-repeat
-    if not chks then
-        chks = output:gsub("(%d)(%d)", g)
-    else
-        chks = chks:gsub("(%d)(%d)", g)
-    end
-until (#chks% 2 == 1)
-print("produces checksum (272) of " .. chks)
-chks = nil
-output = f(input, inputinv, size2, joiner)
-print("input is:\n" .. input)
-print("output after process:\n" .. output)
-repeat
-    if not chks then
-        chks = output:gsub("(%d)(%d)", g)
-    else
-        chks = chks:gsub("(%d)(%d)", g)
-    end
-until (#chks% 2 == 1)
-print("produces checksum (35651584) of " .. chks)
+output = f(input, 272)
+print("checksum for a disk of lengt 272:\n" .. output)
+output = f(input, 35651584)
+print("checksum for a disk of lengt 35651584:\n" .. output)
