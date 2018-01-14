f2ae8c973c85cc801e04284a7b66e14be7c81c06
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-14 00:15:43 -0200

    Update day16.lua
    still not enought

--- 2016/day16.lua
+++ 2016/day16.lua
@@ -1,22 +1,41 @@
 input = "11011110011011101"
+inputinv = ""
+for i = #input, 1, -1 do
+    inputinv = inputinv .. tostring(1 - tonumber(input:sub(i, i)))
+end
 size = 272
-output = input
-function f(a, n)
-    local k = #a
-    local max = math.ceil(math.log(n + 1, 2) - math.log(k + 1, 2))
-    for i = 1, max do
-        local j = #a
-        k = j + 1
-        a = a .. "0"
-        while j > 0 do
-            k = k + 1
-            if k > n then goto skip end
-            a = a .. tostring(1 - tonumber(a:sub(j, j)))
-            j = j - 1
+size2 = 35651584
+joiner = {}
+for i = 1, size2/(#input+1) do
+    if not m or i <= m then
+        joiner[i] = 0
+        if not m then m = 2 end
+    else
+        joiner[i] = 1-joiner[2*m - i]
+        if 2*m <= i + 1 then
+            m = 2*m
         end
     end
-    ::skip::
-    return a
+end
+function f(a, b, n, s)
+    local fstr = ""
+    local t = {}
+    local st = "a"
+    local i = 1
+    while #fstr <= n do
+        if st == "a" then
+            fstr = fstr .. a
+            st = "cb"
+        elseif st == "b" then
+            fstr = fstr .. b
+            st = "ca"
+        else
+            fstr = fstr .. s[i]
+            i = i + 1
+            st = st:sub(2, 2)
+        end
+    end
+    return fstr:sub(1, n)
 end
 function g(d1, d2)
     d1, d2 = tonumber(d1), tonumber(d2)
@@ -26,9 +45,9 @@ function g(d1, d2)
         return 0
     end
 end
-output = f(input, size)
-print("input is " .. input)
-print("output after process " .. output)
+output = f(input, inputinv, size, joiner)
+print("input is:\n" .. input)
+print("output after process:\n" .. output)
 repeat
     if not chks then
         chks = output:gsub("(%d)(%d)", g)
@@ -38,10 +57,9 @@ repeat
 until (#chks% 2 == 1)
 print("produces checksum (272) of " .. chks)
 chks = nil
-size = 35651584
-output = f(input, size)
-print("input is " .. input)
-print("output after process " .. output)
+output = f(input, inputinv, size2, joiner)
+print("input is:\n" .. input)
+print("output after process:\n" .. output)
 repeat
     if not chks then
         chks = output:gsub("(%d)(%d)", g)
