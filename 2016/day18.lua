f45b50ed39fc34b2c9e9017f17493ec6d0a85ef2
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-14 21:38:56 -0200

    Create day18.lua
    part I

---
+++ 2016/day18.lua
@@ -0,0 +1,42 @@
+input = "^..^^.^^^..^^.^...^^^^^....^.^..^^^.^.^.^^...^.^.^.^.^^.....^.^^.^.^.^.^.^.^^..^^^^^...^.....^....^."
+s = 40
+local map = {}
+function f(x, y, z)
+    local r1 = x == 1 and y == 1 and z == 0
+    local r2 = x == 0 and y == 1 and z == 1
+    local r3 = x == 1 and y == 0 and z == 0
+    local r4 = x == 0 and y == 0 and z == 1
+    return r1 or r2 or r3 or r4
+end
+for i = 1, s do
+    map[i] = {}
+    if i == 1 then
+        for j = 1, #input do
+            if input:sub(j, j) == "^" then
+                table.insert(map[i], 1)
+            else
+                table.insert(map[i], 0)
+            end
+        end
+    else
+        local old_map = map[i - 1]
+        for j = 1, #old_map do
+            local left, center, right = (j == 1) and 0 or old_map[j - 1], old_map[j], (j == #old_map) and 0 or old_map[j + 1]
+            if f(left, center, right) then
+                map[i][j] = 1
+            else
+                map[i][j] = 0
+            end
+        end
+    end
+end
+safe = 0
+for i = 1, s do
+    --print(table.concat(map[i]))
+    for j = 1, #map[i] do
+        if map[i][j] == 0 then
+            safe = safe + 1
+        end
+    end
+end
+print("there are " .. safe .. " safe tiles")
