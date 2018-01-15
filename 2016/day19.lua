72996f946fc8fb51815fa0c4c2c601db1573e467
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-15 00:56:16 -0200

    Update day19.lua
    still no part II

--- 2016/day19.lua
+++ 2016/day19.lua
@@ -29,3 +29,23 @@ print("the answer is " .. f(input))
         print(i, sim1(i), f(i))
     end
 --]]
+function sim2(n)
+    local t = {}
+    for i = 1, n do
+        table.insert(t, i)
+    end
+    local i = 1
+    while #t > 1 do
+        if i > #t - math.floor(#t/2) then
+            table.remove(t, (i + math.floor(#t/2) - 1)%(#t) + 1)
+            i = i - 1
+        else
+            table.remove(t, (i + math.floor(#t/2) - 1)%(#t) + 1)
+        end
+        i = i%(#t) + 1
+    end
+    return t[1]
+end
+for i = 1, 100 do
+    print(i, sim2(i))
+end
