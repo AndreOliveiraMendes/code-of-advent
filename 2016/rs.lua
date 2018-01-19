516c38b0fe620830c825ae5d4f186b03602f11bb
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-19 13:32:17 -0200

    Update rs.lua

--- 2016/rs.lua
+++ 2016/rs.lua
@@ -5,7 +5,8 @@ reverse positions 0 through 4
 rotate left 1 step
 move position 1 to position 4
 move position 3 to position 0
-rotate based on position of letter b]]
+rotate based on position of letter b
+rotate based on position of letter d]]
 operation = {}
 for line in input:gmatch("%C+") do
     table.insert(operation, line)
