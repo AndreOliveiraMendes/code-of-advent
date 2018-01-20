65c9861ce820b1ce92e116795d4580f77c1206eb
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-01-20 00:03:11 -0200

    Update day12.lua
    parte 2 cleanup and intrusction opmitization (added a function)

--- 2016/day12.lua
+++ 2016/day12.lua
@@ -8,6 +8,13 @@ end
 function inc(r, list)
     list[r] = list[r] + 1
 end
+function add(r, n, list)
+    if type(n) == "number" then
+        list[r] = list[r] + n
+    else
+        list[r] = list[r] + list[n]
+    end
+end
 function dec(r, list)
     list[r] = list[r] - 1
 end
@@ -38,34 +45,28 @@ function isf(f, ...)
     return false
 end
 input = {{cpy, 1, "a"},
-{cpy, 1, "b"},
-{cpy, 26, "d"},
-{jnz, "c", 2},
-{jnz, 1, 5},
-{cpy, 7, "c"},
-{inc, "d"},
-{dec, "c"},
-{jnz, "c", -2},
-{cpy, "a", "c"},
-{inc, "a"},
-{dec, "b"},
-{jnz, "b", -2},
-{cpy, "c", "b"},
-{dec, "d"},
-{jnz, "d", -6},
-{cpy, 19, "c"},
-{cpy, 14, "d"},
-{inc, "a"},
-{dec, "d"},
-{jnz, "d", -2},
-{dec, "c"},
-{jnz, "c", -5}}
+    {cpy, 1, "b"},
+    {cpy, 26, "d"},
+    {jnz, "c", 2},
+    {jnz, 1, 2},
+    {add, "d", 7},
+    {cpy, "a", "c"},
+    {add, "a", "b"},
+    {cpy, "c", "b"},
+    {dec, "d"},
+    {jnz, "d", -4},
+    {cpy, 0, "c"},
+    {add, "a", 266}
+}
 varlist = {a = 0, b = 0, c = 0, d = 0}
 i = 1
 while (i > 0 and i <= #input) do
     local f, p1, p2 = table.unpack(input[i])
     if isf(f, inc, dec) then
         f(p1, varlist)
+        i = i + 1
+    elseif isf(f, add) then
+        f(p1, p2, varlist)
         i = i + 1
     elseif isf(f, cpy) then
         f(p1, p2, varlist)
@@ -83,6 +84,9 @@ while (i > 0 and i <= #input) do
     if isf(f, inc, dec) then
         f(p1, varlist)
         i = i + 1
+    elseif isf(f, add) then
+        f(p1, p2, varlist)
+        i = i + 1
     elseif isf(f, cpy) then
         f(p1, p2, varlist)
         i = i + 1
@@ -95,7 +99,7 @@ print(varlist.a, varlist.b, varlist.c, varlist.d)
 --[[
     function equivalent
 --]]
-function maineq(a, b, c, d)
+function main(a, b, c, d)
 	a = 1
 	b = 1
 	d = 26
