3d92cc8922dcc343896daa0e55c18e2a867a3577
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-12-18 13:55:13 -0200

    Update day 11.lua
    part II

--- 2018/day 11.lua
+++ 2018/day 11.lua
@@ -5,7 +5,6 @@ function power(x, y, s)
     return d - 5
 end
 grid = {}
-
 serial = 7315
 for x = 1, 300 do
     if not grid[x] then grid[x] = {} end
@@ -13,27 +12,44 @@ for x = 1, 300 do
         grid[x][y] = power(x, y, serial)
     end
 end
-for x = 2, 299 do
-    for y = 2, 299 do
-        local sum = 0
-        for dx = -1, 1 do
-            for dy = -1, 1 do
-                sum = sum + grid[x + dx][y + dy]
+sum = {}
+for i = 1, 300 do
+    for x = 1, 301 - i do
+        if not sum[x] then sum[x] = {} end
+        for y = 1, 301 - i do
+            if not sum[x][y] then sum[x][y] = 0 end
+            for j = 0, i - 1 do
+                sum[x][y] = sum[x][y] + grid[x + i - 1][y + j]
             end
-        end
-        if not mp3 or sum > mp3 then
-            mp3 = sum
-            mp3x = x
-            mp3y = y
-        elseif sum == mp3 then
-            mp3x = nil
-            mp3y = nil
+            for j = 0, i - 2 do
+                sum[x][y] = sum[x][y] + grid[x + j][y + i - 1]
+            end
+            if i == 3 then
+                if not mp3 or sum[x][y] > mp3 then
+                    mp3 = sum[x][y]
+                    mp3x, mp3y = x, y
+                elseif sum[x][y] == mp3 then
+                    mp3x, mp3y = nil, nil
+                end
+            end
+            if not mp or sum[x][y] > mp then
+                mp = sum[x][y]
+                mpx, mpy, mpd = x, y, i
+            elseif sum[x][y] == mp then
+                mpx, mpy = nil, nil
+            end
         end
     end
 end
 --part I
 if mp3x and mp3y then
-    print("top left of 3x3 grid with max power is at " .. mp3x - 1 .. "," .. mp3y - 1)
+    print("top left of 3x3 grid with max power is at " .. mp3x .. "," .. mp3y)
+else
+    print("unexpected behavior")
+end
+--part II
+if mpx and mpy then
+    print("top left of " .. mpd .. "x" .. mpd .. " with max power is at " .. mpx .. "," .. mpy)
 else
     print("unexpected behavior")
 end
