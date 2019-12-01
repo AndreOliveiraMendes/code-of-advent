38a690f0e961155fc68b703228e2dee92d73c0e5
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-01 19:05:36 -0300

    Update day9.lua
    removed hardcoded input

--- 2015/day9.lua
+++ 2015/day9.lua
@@ -1,54 +1,22 @@
-input = [[Tristram to AlphaCentauri = 34
-Tristram to Snowdin = 100
-Tristram to Tambi = 63
-Tristram to Faerun = 108
-Tristram to Norrath = 111
-Tristram to Straylight = 89
-Tristram to Arbre = 132
-AlphaCentauri to Snowdin = 4
-AlphaCentauri to Tambi = 79
-AlphaCentauri to Faerun = 44
-AlphaCentauri to Norrath = 147
-AlphaCentauri to Straylight = 133
-AlphaCentauri to Arbre = 74
-Snowdin to Tambi = 105
-Snowdin to Faerun = 95
-Snowdin to Norrath = 48
-Snowdin to Straylight = 88
-Snowdin to Arbre = 7
-Tambi to Faerun = 68
-Tambi to Norrath = 134
-Tambi to Straylight = 107
-Tambi to Arbre = 40
-Faerun to Norrath = 11
-Faerun to Straylight = 66
-Faerun to Arbre = 144
-Norrath to Straylight = 115
-Norrath to Arbre = 135
-Straylight to Arbre = 127]]
-loc = {}
-for word in string.gmatch(input, "%a+") do
-    if word ~= "to" then
-        local continue = false
-        for i, s in pairs(loc) do
-            if s == word then continue = true end
-        end
-        if not continue then
-            table.insert(loc, word)
-        end
+function table_nrinsert(t, v)
+    for i, s in pairs(t) do
+        if s == v then return false end
     end
+    return true
 end
+loc = {}
 dist = {}
-for line in string.gmatch(input, "%C+") do
+for line in io.lines() do
     local t = {}
     for word in string.gmatch(line, "%a+") do
         if word ~= "to" then
+            table_nrinsert(loc, word)
             table.insert(t, word)
         end
     end
     local loc1, loc2 = t[1], t[2]
-    if not dist[loc1] then dist[loc1] = {} end
-    if not dist[loc2] then dist[loc2] = {} end
+    dist[loc1] = dist[loc1] or {}
+    dist[loc2] = dist[loc2] or {}
     dist[loc1][loc2], dist[loc2][loc1] = tonumber(string.match(line, "%d+")), tonumber(string.match(line, "%d+"))
 end
 function getmind(loc, distl, used)
