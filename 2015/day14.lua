1658b064fd466022e9c1ce94a8b6f06f4ec71f7a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-01 22:45:29 -0300

    Update day14.lua
    removed hardcoded input

--- 2015/day14.lua
+++ 2015/day14.lua
@@ -1,19 +1,11 @@
-input = [[Dancer can fly 27 km/s for 5 seconds, but then must rest for 132 seconds.
-Cupid can fly 22 km/s for 2 seconds, but then must rest for 41 seconds.
-Rudolph can fly 11 km/s for 5 seconds, but then must rest for 48 seconds.
-Donner can fly 28 km/s for 5 seconds, but then must rest for 134 seconds.
-Dasher can fly 4 km/s for 16 seconds, but then must rest for 55 seconds.
-Blitzen can fly 14 km/s for 3 seconds, but then must rest for 38 seconds.
-Prancer can fly 3 km/s for 21 seconds, but then must rest for 40 seconds.
-Comet can fly 18 km/s for 6 seconds, but then must rest for 103 seconds.
-Vixen can fly 18 km/s for 5 seconds, but then must rest for 84 seconds.]]
 com = {}
-for line in string.gmatch(input, "%C+") do
+for line in io.lines() do
     local name = string.match(line, "%w+")
-    local v, ft, rt
+    local t = {}
     for d in string.gmatch(line, "%d+") do
-        if not v then v = d elseif not ft then ft = d else rt = d end
+        table.insert(t, tonumber(d))
     end
+    local v, ft, rt = table.unpack(t)
     table.insert(com, {name, v, ft, rt})
 end
 function race(total_time, mode)
