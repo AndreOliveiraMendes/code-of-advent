ef64043aabf59a0a2a2884fe3804f6274599ea9d
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-03 19:03:00 -0300

    Create day_3.lua
    part i done

---
+++ 2019/day_3.lua
@@ -0,0 +1,50 @@
+function man_dist(x1, y1, x2, y2)
+    if x2 then
+        return math.abs(x1 - x2) + math.abs(y1 - y2)
+    else
+        return math.abs(x1) + math.abs(y1)
+    end
+end
+function deploy_wire(map, input)
+    local x, y = 0, 0
+    map = map or {}
+    map[0] = map[0] or {}
+    map[0][0] = true
+    for w in input:gmatch("%a%d+") do
+        local dir, n = w:sub(1, 1), tonumber(w:sub(2))
+        local dx, dy = (dir == "R") and 1 or (dir == "L") and -1 or 0, (dir == "U") and 1 or (dir == "D") and -1 or 0
+        for i = 1, n do
+            x, y = x + dx, y + dy
+            map[x] = map[x] or {}
+            map[x][y] = true
+        end
+    end
+end
+function get_connection_min_dist(map1, map2)
+    local min_dist
+    for x in pairs(map1) do
+        for y in pairs(map1[x]) do
+            if x~=0 or y~= 0 then
+                if map2[x] and map2[x][y] then
+                    min_dist = not min_dist and man_dist(x, y) or math.min(man_dist(x, y), min_dist) 
+                end
+            end
+        end
+    end
+    return min_dist
+end
+map1, map2 = {}, {}
+segond = false
+for line in io.lines() do
+    if not segond then
+        segond = true
+        deploy_wire(map1, line)
+    else
+        deploy_wire(map2, line)
+    end
+end
+--part I
+min_dist = get_connection_min_dist(map1, map2)
+if min_dist then
+    print("minimal distance is " .. min_dist)
+end
