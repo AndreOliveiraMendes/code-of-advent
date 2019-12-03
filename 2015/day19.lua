e0e9ad8b6ed2da2bf3e1592446496450c5e8ef1a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-03 14:30:50 -0200

    puzzle bf

--- 2015/day19.lua
+++ 2015/day19.lua
@@ -13,6 +13,7 @@ function tinsertr(t, value)
     if chk then table.insert(t, value) end
 end
 elemental_replace = {}
+inv_elemental_replace = {}
 elements = {}
 for line in io.lines() do
     if line:match("=>") then
@@ -24,6 +25,7 @@ for line in io.lines() do
         tinsertr(elements, e1)
         tinsertr(elements, e2)
         table.insert(elemental_replace, {e1, e2})
+        table.insert(inv_elemental_replace, {e2, e1})
     elseif line:match("%a") then
         input_element = line
     end
@@ -36,10 +38,37 @@ function replace(distincts_mol, input_element, elemental_replace)
         while i do
             local mol = replace_char(i, o, input_element, s_out)
             tinsertr(distincts_mol, mol)
-            i, o = input_element:find(s_in, o + 1)
+            i, o = input_element:find(s_in, i + 1)
         end
     end
 end
 --part I
-replace(distincts_mol, input_element, elemental replace)
+replace(distincts_mol, input_element, elemental_replace)
 print("there are " .. #distincts_mol .. " distincts molecules possible")
+memory = {}
+function find(input_element, output_element, elemental_replace, memory)
+    local step
+    for i, s in pairs(elemental_replace) do
+        local s_in, s_out = s[1], s[2]
+        local i, o = input_element:find(s_in)
+        while i do
+            local mol = replace_char(i, o, input_element, s_out)
+            if mol == output_element then
+                return 1
+            end
+            local out
+            if memory and memory[mol] then
+                out = memory[mol]
+            else
+                out = find(mol, output_element, elemental_replace, memory)
+            end
+            --print(mol, out)
+            if out and (not step or out + 1 < step) then
+                step = out + 1
+            end
+            i, o = input_element:find(s_in, i + 1)
+        end
+    end
+    return step
+end
+print("min number of steps is " .. find(input_element, "e", inv_elemental_replace, memory))
