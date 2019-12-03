e6afc4686b5db006d5de0d3c13e42f952776fb70
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-02 21:03:48 -0300

    Update day20.lua
    removed hardcoded input

--- 2015/day20.lua
+++ 2015/day20.lua
@@ -1,53 +1,3 @@
---[[
-    function d(n)
-        local sum = 0
-        for i = 1, n do
-            if n % i == 0 then
-                sum = sum + i
-            end
-        end
-        return sum
-    end
-    function dm(n)
-        local sum = 0
-        for i = 1, n do
-            if n <= 50*i and n%i == 0 then
-                sum = sum + i
-            end
-        end
-        return sum
-    end
-    function d(n)
-        local sum = 0
-        for i = 1, math.sqrt(n) do
-            if n % i == 0 then
-                if i == math.sqrt(n) then
-                    sum = sum + i
-                else
-                    sum = sum + i + n/i
-                end
-            end
-        end
-        return sum
-    end
-    function dm(n)
-        local sum = 0
-        for i = n, math.sqrt(n), -1 do
-            if n <= 50*i and n%i == 0 then
-                if i == math.sqrt(n) then
-                    sum = sum + i
-                else
-                    sum = sum + i
-                    local j = n/i
-                    if n <= 5*j then
-                        sum = sum + j
-                    end
-                end
-            end
-        end
-        return math.floor(sum)
-    end
---]]
 function d(n)
     local sum1, sum2 = 0, 0
     for i = 1, math.sqrt(n) do
@@ -71,7 +21,7 @@ function d(n)
     end
     return sum1, sum2
 end
-input = 29000000
+input = tonumber(io.read())
 dm = {}
 function f1(n)
     if not dm[n] then
