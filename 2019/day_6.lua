7b424f70b624726fcef3968385b72fb49bf7d6c0
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-06 14:57:23 -0300

    Update day_6.lua
    finished part 2

--- 2019/day_6.lua
+++ 2019/day_6.lua
@@ -4,6 +4,8 @@ for line in io.lines() do
     map[p1] = map[p1] or {}
     map[p2] = map[p2] or {}
     map[p2].orbit = map[p1]
+    map[p1].orbited = map[p1].orbited or {}
+    table.insert(map[p1].orbited, map[p2])
 end
 function get_checksum(p)
     local pb = p.orbit
@@ -19,3 +21,43 @@ for _, p in pairs(map) do
 end
 --part i
 print("part i answer: " .. sum)
+--part ii
+function get_min_path(map, origin, destine)
+    if origin == destine then
+        return true, 0
+    else
+        if origin.visited then
+            return false, 0
+        else
+            local min
+            origin.visited = true
+            if origin.orbit then
+                local found, dist = get_min_path(map, origin.orbit, destine)
+                if found then
+                    min = dist + 1
+                end
+            end
+            if origin.orbited then
+                for _, p in pairs(origin.orbited) do
+                    local found, dist = get_min_path(map, p, destine)
+                    if found and (not min or (dist + 1) < min) then
+                        min = dist + 1
+                    end
+                end
+            end
+            origin.visited = nil
+            if min then
+                return true, min
+            else
+                return false, 0
+            end
+        end
+    end
+end
+--part II
+found, dist = get_min_path(map, map.YOU.orbit, map.SAN.orbit)
+if found then
+    print("part ii answer: " .. dist)
+else
+    print("unknow error")
+end
