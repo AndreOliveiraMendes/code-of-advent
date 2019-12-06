19d5938bf3ca6d69eecd6e962c27ca9e5a8c4c79
AndreOliveiraMendes <ao_mendes@hotmail.com>
2019-12-05 22:46:42 -0300

    Update day_5.lua
    part 2

--- 2019/day_5.lua
+++ 2019/day_5.lua
@@ -5,42 +5,75 @@ for n in string.gmatch(io.read(), "-?%d+") do
     table.insert(t, n)
     table.insert(input, n)
 end
+function get_mode(op, bit)
+    return math.floor(op/(10^bit))%10
+end
+function get_param(t, pos, param)
+    return t[pos + param]
+end
+function get_value(t, op, pos, param)
+    local mode, p = get_mode(op, param + 1), get_param(t, pos, param)
+    if mode == 0 then
+        return t[p + 1]
+    elseif mode == 1 then
+        return p
+    else
+        print("error, invalide mode:" .. mode)
+        return 0
+    end
+end
 function read(t, pos, inp, out)
     local op = t[pos]
     local opcode = op%100
-    if opcode == 99 then return false end
-    if opcode == 1 or opcode == 2 then
-        local v1, v2, v3
-        if math.floor(op/(10^2))%10 == 0 then
-            local i1 = t[pos + 1] + 1
-            v1 = t[i1]
-        else
-            v1 = t[pos + 1]
-        end
-        if math.floor(op/(10^3))%10 == 0 then
-            local i2 = t[pos + 2] + 1
-            v2 = t[i2]
-        else
-            v2 = t[pos + 2]
-        end
+    if opcode == 99 then
+        return false
+    elseif opcode == 1 or opcode == 2 then
+        local v1, v2, v3, p3
+        v1 = get_value(t, op, pos, 1)
+        v2 = get_value(t, op, pos, 2)
         v3 = (opcode == 1) and (v1 + v2) or (v1 * v2)
-        local i3 = t[pos + 3] + 1
-        t[i3] = v3
+        p3 = get_param(t, pos, 3)
+        t[p3 + 1] = v3
         return true, 4
     elseif opcode == 3 then
-        local i1 = t[pos + 1] + 1
-        t[i1] = inp
+        local p1
+        p1 = get_param(t, pos, 1)
+        t[p1 + 1] = inp
         return true, 2
     elseif opcode == 4 then
-        local i1 = t[pos + 1] + 1
-        table.insert(out, t[i1])
+        local p1
+        p1 = get_param(t, pos, 1)
+        table.insert(out, t[p1 + 1])
         return true, 2
+    elseif opcode == 5 or opcode == 6 then
+        local v1
+        v1 = get_value(t, op, pos, 1)
+        if (opcode == 5 and v1 ~= 0) or (opcode == 6 and v1 == 0) then
+            local v2 = get_value(t, op, pos, 2)
+            return true, 1 + v2 - pos
+        else
+            return true, 3
+        end
+    elseif opcode == 7 or opcode == 8 then
+        local v1, v2, v3
+        v1 = get_value(t, op, pos, 1)
+        v2 = get_value(t, op, pos, 2)
+        p3 = get_param(t, pos, 3)
+        if (opcode == 7 and v1 < v2) or (opcode == 8 and v1 == v2) then
+            t[p3 + 1] = 1
+        else
+            t[p3 + 1] = 0
+        end
+        return true, 4
+    else
+        print("invalid opcode:" .. op)
+        return false
     end
 end
 function output(t, input)
     local pos = 0
     local out = {}
-    while pos < #t do
+    while pos >= 0 and pos < #t do
         local succed, dpos = read(t, pos + 1, input, out)
         if succed then
             pos = pos + dpos
@@ -57,5 +90,9 @@ function reset(t1, t2)
 end
 --part I
 out = output(t, 1)
-print(out[#out])
+print("part I answer:" .. out[#out])
+--part II
 reset(t, input)
+out = output(t, 5)
+print("part II answer:" .. out[#out])
+print(out[#out])
